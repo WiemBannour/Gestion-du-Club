db1b6b2baf54726698491d92e909d4ee80475964
Wiem Bannour <147947620+WiemBannour@users.noreply.github.com>
2023-11-29 21:35:00 +0100

    Add files via upload

---
+++ userController.js
@@ -0,0 +1,29 @@
+const User = require('../models/User');
+
+const createUser = async (req, res) => {
+    // Logique pour créer un utilisateur
+};
+
+const getAllUsers = async (req, res) => {
+    // Logique pour obtenir tous les utilisateurs
+};
+
+const getUserById = async (req, res) => {
+    // Logique pour obtenir un utilisateur par ID
+};
+
+const updateUser = async (req, res) => {
+    // Logique pour mettre à jour un utilisateur
+};
+
+const deleteUser = async (req, res) => {
+    // Logique pour supprimer un utilisateur
+};
+
+module.exports = {
+    createUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
+};
